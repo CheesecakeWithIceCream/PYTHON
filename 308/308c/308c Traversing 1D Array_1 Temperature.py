26cb8547567a3e717a4bec2be04553c5a0873137
CheesecakeWithIceCream <s.neupane@albynschool.org>
2024-10-04 12:26:49 +0100

    ho

---
+++ 308/308c/308c Traversing 1D Array_1 Temperature.py
@@ -0,0 +1,8 @@
+
+items = [4,9,6,13,8,3,7]
+total_temp = 0
+for index in range(7):
+    total_temp += items[index]
+avg_temp = total_temp/7
+print(avg_temp)
+
---
+++ 308/308c/308c Traversing 1D Array_2 Dog Food.py
@@ -0,0 +1,5 @@
+items = [4,9,6,13,8]
+total_food = 0
+for index in range(7):
+    total_food += items[index]
+print(total_food)
